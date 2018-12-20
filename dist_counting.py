7d4f9bc4d5d0b7f532d83839c80ef05884af2f50
maribor666 <spekaoleh@ukr.net>
2018-12-20 20:24:31 +0200

    add:st distancec couning. pattern for db gap filling

---
+++ dist_counting.py
@@ -0,0 +1,31 @@
+import geopy.distance as gp
+from pprint import pprint
+import json
+
+coors_path = "./station_coors.txt"
+
+def main():
+    content = open(coors_path, mode='r').readlines()
+    stations = []
+    for line in content:
+        splited = line.split()
+        st = {}
+        st['name'] = splited[0]
+        st['lon'] = splited[1]
+        st['lat'] = splited[2]
+        stations.append(st)
+    st_data = {}
+    for st1 in stations:
+        st_data[st1['name']] = {}
+        for st2 in stations:
+            c1 = (float(st1['lon']), float(st1['lat']))
+            c2 = (float(st2['lon']), float(st2['lat']))
+            distance = round(gp.vincenty(c1, c2).km, 2)
+            if distance != 0:
+                st_data[st1['name']][st2['name']] = distance
+    with open("./station_distances.json", 'w+') as fout:
+        json.dump(st_data, fout)
+
+
+if __name__ == '__main__':
+    main()
--- fill_db_gaps.py
+++ fill_db_gaps.py
@@ -2,37 +2,31 @@ import sqlite3
 from datetime import datetime
 from pprint import pprint
 from dateutil.relativedelta import relativedelta
+import json
 
 db_path = './db.sqlite'
+dist_path = './station_distances.json'
 start_date = datetime(year=2011, month=1, day=1)
 end_date = datetime.now()
 end_date = datetime(year=end_date.year, month=end_date.month, day=end_date.day)
 
-distances = {
-
-}
 
 def main():
     # upd db before filling!!!
-
     con = sqlite3.connect(db_path)
     cursor = con.cursor()
-
     days_num = (end_date - start_date).days + 1
     res = cursor.execute("""SELECT name FROM sqlite_master WHERE type='table';""").fetchall()
     table_names = [el[0] for el in res]
-    pprint(table_names)
-    # st_to_upd = select_st_to_upd(cursor, days_num, table_names)
-
-    # for st in st_to_upd:
-    # fill_gaps(cursor, st_to_upd[1])
-
-    # print(end_date)
+    st_to_upd = select_st_to_upd(cursor, days_num, table_names)
+    st_distances = json.load(open(dist_path, mode='r'))
+    for st in st_to_upd:
+        fill_gaps(cursor, st, st_distances)
     con.commit()
     con.close()
 
 
-def fill_gaps(cursor, st):
+def fill_gaps(cursor, st, st_distances):
     print(st)
     one_day = relativedelta(days=1)
     curr_date = start_date
@@ -46,7 +40,30 @@ def fill_gaps(cursor, st):
         if not res:
             gap_days.append(curr_date)
         curr_date += one_day
+    data_to_fill = None
+    for gap_day in gap_days:
+        while data_to_fill is None:
+            data_to_fill = get_data_to_fill(st, cursor, gap_day, st_distances)
+        print(data_to_fill)
+        data_to_fill = None
 
+
+def get_data_to_fill(st, cursor, gap_day, st_distances):
+    data = None
+    i = 0
+    while data is not False:
+        n_nearest_st = find_nearest_st(st, st_distances, i=i)
+        print(n_nearest_st)
+        # cursor.execute("""
+        #
+        # """).fetchall()
+
+        i += 1
+    return None
+
+def find_nearest_st(st, st_distances, i=0):
+    stations = list(st_distances[st])
+    print(stations)
 
 
 def select_st_to_upd(cursor, days_num, table_names):
