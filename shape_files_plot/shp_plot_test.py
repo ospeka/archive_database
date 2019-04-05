4f516e63b0b857c8cc1d6bf8f03d48781fbd9058
unknown <spekaoleh@ukr.net>
2019-04-05 13:42:20 +0300

    add: polygons parse setup

--- shape_files_plot/shp_plot_test.py
+++ shape_files_plot/shp_plot_test.py
@@ -1,16 +1,86 @@
-import shapely
+from pprint import pprint
 
-# shp_file_path = "./Subbasins116.shp"
+subbasins_path = "./polygons.txt"
 
 def main():
-	# lines = open("./output.sub", mode='r').readlines()
-	# for line in lines[9:9+116]:
-	# 	# read column mg/L
-	# 	record = float(line.split()[-4])
-	# 	print(record, ',', sep='')
-	pass
+	subbasin_polygons = open(subbasins_path, mode='r').readlines()
+	# pprint(subbasin_polygons[:10])
+	subbasins = parse_subbasins(subbasin_polygons)
+	print(len(subbasins))
+	
 
 
+def parse_subbasins(lines):
+	l = len(lines)
+	start = 0
+	subbasins = []
+	for i in range(l):
+		try:
+			if lines[i + 1] == "" and lines[i + 2] == "":
+				end = i + 1
+				subbasin = parse_subbasin(lines, start, end)
+				subbasins.append(subbasin)
+				start = i + 2
+		except IndexError:
+			end = l - 1
+			subbasin = parse_subbasin(lines, start, end)
+			subbasins.append(subbasin)
+			break
+	return subbasins
+
+def parse_subbasin(lines, start, end):
+	to_parse = lines[start:end]
+	subbasin = Subbasin()
+	pol = Polygon()
+	is_one_pol = True
+	for line in to_parse:
+		if line != "":
+			splited = line.split()
+			x, y = splited[0], splited[1]
+			pol.add_point(x, y)
+		else:
+			is_one_pol = False
+			subbasin.one_pol = False
+			subbasin.pol.append(pol)
+			pol = Polygon()
+	if is_one_pol:
+		subbasin.pol = pol
+	return subbasin
+
+
+
+class Subbasin(object):
+	"""Subbasin represents actually subbasin.
+		If subbasin contains only one polygon then
+		attr pol is type of Polygon. If subbasins contains
+		many polygons then attr poll is list of objects type of
+		Polygon
+	"""
+	def __init__(self):
+		super(Subbasin, self).__init__()
+		self.pol = []
+		self.one_pol = True
+
+	def plot(self, ax):
+		pass
+
+class Polygon(object):
+	"""docstring for Polygon"""
+	def __init__(self):
+		super(Polygon, self).__init__()
+		self.points = []
+
+	def add_point(self, x, y):
+		self.points.append(Point(x, y))
+
+class Point(object):
+	"""docstring for Point"""
+	def __init__(self, x, y):
+		super(Point, self).__init__()
+		self.x = x
+		self.y = y
+		
+	
 
 if __name__ == "__main__":
 	main()
--- version.py
+++ version.py
@@ -1,10 +1,12 @@
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
-x = [el for el in range(100)]
-y = [xi ** 2 for xi in x]
+# x = [el for el in range(100)]
+# y = [xi ** 2 for xi in x]
 
-fig, axes = plt.subplots(1,1)
-axes.plot(x, y)
-axes.legend(['Y(x)'])
-plt.show()
-print(type(axes))
+# fig, axes = plt.subplots(1,1)
+# axes.plot(x, y)
+# axes.legend(['Y(x)'])
+# plt.show()
+# print(type(axes))
+
+print(bool(''))
