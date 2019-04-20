2d038245968f0ecedaf24504f971dff620b521fa
maribor666 <spekaoleh@ukr.net>
2019-04-20 19:47:52 +0300

    add: parse output finished

--- parse_output/parse_output.py
+++ parse_output/parse_output.py
@@ -1,35 +1,42 @@
-from pprint import pprint
-
 # path to output file
 p = "./output.sub"
 
-def main():
-	file = open(p, mode='r')
-	lines = file.readlines()
-	data_lines = lines[9:]
-	col_names = lines[8].split()
+def parse_output(path):
+    file = open(path, mode='r')
+    lines = file.readlines()
+    data_lines = lines[9:]
+    col_names = lines[8].split()
 
-	subbasins = [set_up_dict(col_names) for _ in range(116)]
-	days = data_lines[::116]
-	days_num = len(days)
-	
-	for day in days:
-		for subbasin in subbasins:
-			print(day)
-			print(subbasin)
-			exit()
-
-
-
-
+    subbasins = [set_up_dict(col_names) for _ in range(116)]
+    i = 0
+    flag = 0
+    for line in data_lines:
+        for col_name, meas in zip(col_names, line.split()):
+            try:
+                if flag == 1 and col_name == 'mg/L':
+                    subbasins[i]['mg/L 2'].append(float(meas))
+                    flag = 0
+                    continue
+                if col_name == 'mg/L':
+                    flag = 1
+                subbasins[i][col_name].append(float(meas))
+            except:
+                subbasins[i][col_name].append(meas)
+        i += 1
+        if i == 116:
+            i = 0
+    return subbasins
 
 
 def set_up_dict(col_names):
-	subbasin = {}
-	for col_name in col_names:
-		subbasin[col_name] = []
-	return subbasin
+    subbasin = {}
+    for col_name in col_names:
+        subbasin[col_name] = []
+    subbasin['mg/L 2'] = []
+    return subbasin
 
+def get_vals():
+    pass
 
 if __name__ == '__main__':
-	main()
+    parse_output(p)
